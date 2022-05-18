cf411420899aa49abef2852ead9e67702d3fa3ab
Andrew Wong <wong.andrewko@gmail.com>
2022-05-17 18:15:53 -0700

    Add function to create URIs that grab board game data by id.
    This creates bgg_data.py, at which more functionality will be added later.
    Also add test_bgg_data.py for testing with pytest.

---
+++ bgg_data.py
@@ -0,0 +1,49 @@
+def get_game_uri(ids=[], filter_basegame=True, filter_expansion=False, stats=True):
+    """Construct a URI to get one or more games based on their id.
+
+    Parameters
+    ----------
+    ids : list, optional
+        list of numerical ids of the board games, by default []
+    filter_basegame : boolean, optional
+        filter for base board games, by default True.
+    filter_expansion : boolean, optional
+        filter for board game expansions, by default False.
+    stats : boolean, optional
+        include statistics i.e. "stats=1" in the query.
+
+    Returns
+    -------
+    str URI
+        URI accessing board games with specified ids.
+    """
+
+    # Board Game Geek XML API2 endpoint for retrieving "thing" objects
+    BASE_API_ENDPOINT = "https://boardgamegeek.com/xmlapi2/thing?"
+    # Thing types
+    # Non-expansion games
+    BASE_GAME_TYPE = "boardgame"
+    # Expansions
+    EXPANSION_TYPE = "boardgameexpansion"
+
+    # Set up the output
+    uri = BASE_API_ENDPOINT
+
+    # Add type filters
+    uri += "type="
+    if filter_basegame:
+        uri += BASE_GAME_TYPE + ','
+    if filter_expansion:
+        uri += EXPANSION_TYPE + ','
+    uri += '&'
+
+    # Add stats query
+    if stats:
+        uri += "stats=1&"
+
+    # Add ids
+    uri += "id="
+    # i.e. [1,2] -> '1,2'
+    uri += ','.join([str(i) for i in ids])
+
+    return uri
---
+++ test_bgg_data.py
@@ -0,0 +1,38 @@
+import bgg_data as b
+
+
+def test_get_game_uri():
+    ids = [1, 2]
+    "https://boardgamegeek.com/xmlapi2/thing?"
+
+    # Get base games only
+    assert b.get_game_uri(
+        ids) == "https://boardgamegeek.com/xmlapi2/thing?type=boardgame,&stats=1&id=1,2"
+
+    # Get expansions only
+    assert b.get_game_uri(
+        ids,
+        filter_basegame=False,
+        filter_expansion=True) == "https://boardgamegeek.com/xmlapi2/thing?type=boardgameexpansion,&stats=1&id=1,2"
+
+    # Get base + expansions
+    assert b.get_game_uri(
+        ids,
+        filter_basegame=True,
+        filter_expansion=True) == "https://boardgamegeek.com/xmlapi2/thing?type=boardgame,boardgameexpansion,&stats=1&id=1,2"
+
+    # No filter
+    assert b.get_game_uri(
+        ids,
+        filter_basegame=False,
+        filter_expansion=False) == "https://boardgamegeek.com/xmlapi2/thing?type=&stats=1&id=1,2"
+
+    # no stats
+    assert b.get_game_uri(
+        ids,
+        stats=False) == "https://boardgamegeek.com/xmlapi2/thing?type=boardgame,&id=1,2"
+
+    # single id only
+    assert b.get_game_uri(
+        ids=[35],
+        stats=False) == "https://boardgamegeek.com/xmlapi2/thing?type=boardgame,&id=35"
