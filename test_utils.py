954a12eda090c63f8e75635048b9d3059656e4a0
Andrew Wong <wong.andrewko@gmail.com>
2022-05-24 16:40:53 -0700

    Add read_xml_file function w/ tests.

--- test_utils.py
+++ test_utils.py
@@ -1,13 +1,21 @@
 import utils as u
 import lxml.etree as etree
 
+TEST_DATA_FILEPATH = 'test_data.xml'
+
 # Use bytes as etree.fromstring only allows encoding declaration
 # when it is bytes.
-with open('test_data.xml', 'rb') as f:
+with open(TEST_DATA_FILEPATH, 'rb') as f:
     xml_data = etree.fromstring(f.read())
 
 
-# These test the Extractor class
+def test_read_xml_file(file_path=TEST_DATA_FILEPATH):
+    root = u.read_xml_file(file_path)
+    assert root.tag == 'items'
+    assert root.attrib.keys()[0] == 'termsofuse'
+
+
+# extractor_* test the Extractor class
 def test_extractor_id(xml_data=xml_data):
     item = u.Extractor(xml_data[0])
     assert item.extract_id() == 28192
--- utils.py
+++ utils.py
@@ -1,3 +1,27 @@
+import lxml.etree as etree
+from pathlib import Path
+
+
+def read_xml_file(file_path: str) -> etree.Element:
+    """Read an xml file using lxml and get the root element.
+
+    Parameters
+    ----------
+    file_path : str
+        Location of the xml file.
+
+    Returns
+    -------
+    etree.Element
+        The root element of the xml file.
+    """
+    # Read xml data
+    # etree raises error for read_text
+    xml_data = Path(file_path).read_bytes()
+    # return root element
+    return etree.fromstring(xml_data)
+
+
 class Extractor():
     """
     Handles extracting various XML attributes and values
