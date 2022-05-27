0bc924d742c7160c48bf6736c58679aaf8048ed8
Andrew Wong <wong.andrewko@gmail.com>
2022-05-26 21:02:22 -0700

    Add test for utils.extract_xml.

--- test/test_utils.py
+++ test/test_utils.py
@@ -6,6 +6,8 @@ import pytest
 # as the first item, and additional items for error handling testing.
 # Path is as if `python -m pytest` is being run in the root folder.
 TEST_DATA_FILEPATH = 'test/test_data.xml'
+# Just a single item
+TEST_DATA_SINGLE_FILEPATH = 'test/test_data_single.xml'
 
 # Use bytes as etree.fromstring only allows encoding declaration
 # when it is bytes.
@@ -36,3 +38,10 @@ def test_extractor_year_published(xml_data=xml_data):
     item = u.Extractor(xml_data[1])
     with pytest.raises(TypeError):
         item.extract_year_published()
+
+
+def test_extract_xml(file_path=TEST_DATA_SINGLE_FILEPATH):
+    data = u.extract_xml(file_path=file_path, id=True, year_published=True)
+    print(data)
+    assert data['id'] == [28192]
+    assert data['year_published'] == [1989]
