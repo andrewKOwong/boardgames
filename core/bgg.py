e77130109f017367b7f610bc1f49701fe92f0fa3
Andrew Wong <wong.andrewko@gmail.com>
2022-08-21 21:46:37 -0700

    Refactor `generate_game_uri` into Retriever class

--- core/bgg.py
+++ core/bgg.py
@@ -4,67 +4,6 @@ import json
 from time import sleep
 import requests
 
-def generate_game_uri(
-    ids: list = None,
-    filter_basegame: bool = True,
-    filter_expansion: bool = False,
-    stats: bool = True,
-    base_api_endpoint: str = "https://boardgamegeek.com/xmlapi2/thing?"
-    ) -> str:
-    """Construct a URI to get one or more games based on their id.
-
-    Parameters
-    ----------
-    ids : list, optional
-        list of numerical ids of the board games, by default []
-    filter_basegame : boolean, optional
-        filter for base board games, by default True.
-    filter_expansion : boolean, optional
-        filter for board game expansions, by default False.
-    stats : boolean, optional
-        include statistics i.e. "stats=1" in the query.
-    base_api_endpoint : str, optional
-        Board Game Geek XML API2 endpoint for retrieving "thing" objects.
-        Can be swapped for testing.
-
-    Returns
-    -------
-    str URI
-        URI accessing board games with specified ids.
-    """
-    # Thing types
-    # Non-expansion games
-    BASE_GAME_TYPE = "boardgame"
-    # Expansions
-    EXPANSION_TYPE = "boardgameexpansion"
-
-    # Instantiated empty list
-    if ids is None:
-        ids = []
-
-    # Set up the output
-    uri = base_api_endpoint
-
-    # Add type filters
-    uri += "type="
-    if filter_basegame:
-        uri += BASE_GAME_TYPE + ','
-    if filter_expansion:
-        uri += EXPANSION_TYPE + ','
-    uri += '&'
-
-    # Add stats query
-    if stats:
-        uri += "stats=1&"
-
-    # Add ids
-    uri += "id="
-    # i.e. [1,2] -> '1,2'
-    uri += ','.join([str(i) for i in ids])
-
-    return uri
-
-
 def xml_collater():
     pass
 
@@ -77,6 +16,7 @@ class Retriever:
     PROGRESS_KEY_IDS = "ids"
     PROGRESS_KEY_STATUS = "status"
     PROGRESS_KEY_LAST_ACCESSED = "last_accessed"
+    BASE_API = "https://boardgamegeek.com/xmlapi2/thing?"
 
 
     def __init__(self, save_path):
@@ -117,7 +57,9 @@ class Retriever:
                 # Try the request, but pause 10 min if no internet
                 while True:
                     try:
-                        uri = generate_game_uri(batch[self.PROGRESS_KEY_IDS])
+                        uri = self.generate_game_uri(
+                            batch[self.PROGRESS_KEY_IDS]
+                            )
                         r = self.api_request(uri)
                         break
                     except requests.ConnectionError:
@@ -187,3 +129,57 @@ class Retriever:
     def check_progress_file_exists(self) -> bool:
         """True if save file already exists."""
         return os.path.isfile(self.save_path)
+
+    def generate_game_uri(
+        self,
+        ids: list = None,
+        filter_basegame: bool = True,
+        filter_expansion: bool = False,
+        stats: bool = True,
+        base_api_endpoint: str = BASE_API
+    ) -> str:
+        """Construct a URI to get one or more games based on their id.
+
+        Args:
+            ids (list, optional): numerical ids of the board games.
+                Defaults to None.
+            filter_basegame (bool, optional): filter for base board games.
+                Defaults to True.
+            filter_expansion (bool, optional): filter for
+                board game expansions. Defaults to False.
+            stats (bool, optional): include statistics
+                i.e. "stats=1" in the query. Defaults to True.
+            base_api_endpoint (str, optional): Board Game Geek XML API2
+                endpoint for retrieving "thing" objects. Defaults to BASE_API.
+
+        Returns:
+            str:  URI accessing board games with specified ids.
+        """
+        # Thing types
+        BASE_GAME_TYPE = "boardgame" # i.e. non expansions
+        EXPANSION_TYPE = "boardgameexpansion"
+
+        # Instantiated empty list
+        if ids is None:
+            ids = []
+
+        # Set up the output
+        uri = base_api_endpoint
+
+        # Add type filters
+        uri += "type="
+        if filter_basegame:
+            uri += BASE_GAME_TYPE + ','
+        if filter_expansion:
+            uri += EXPANSION_TYPE + ','
+        uri += '&'
+
+        # Add stats query
+        if stats:
+            uri += "stats=1&"
+
+        # Add ids
+        uri += "id="
+        # i.e. [1,2] -> '1,2'
+        uri += ','.join([str(i) for i in ids])
+        return uri
