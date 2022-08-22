c72335d071e8722d4bf3d2d4cc0faff4ff8613ac
Andrew Wong <wong.andrewko@gmail.com>
2022-08-21 20:58:59 -0700

    Add sketch of `retrieve_all` func

--- core/bgg.py
+++ core/bgg.py
@@ -1,5 +1,8 @@
 import os
+import sys
 import json
+from time import sleep
+import requests
 
 def generate_game_uri(
     ids: list = None,
@@ -68,25 +71,85 @@ def xml_collater():
 
 class Retriever:
     MAX_ID = 362383
-    PROGRESS_COMPLETE = 'complete'
-    PROGRESS_INCOMPLETE = 'incomplete'
-    PROGRESS_QUEUED = 'queued'
+    PROGRESS_STATUS_COMPLETE = 'complete'
+    PROGRESS_STATUS_INCOMPLETE = 'incomplete'
+    PROGRESS_STATUS_QUEUED = 'queued'
+    PROGRESS_KEY_IDS = "ids"
+    PROGRESS_KEY_STATUS = "status"
+    PROGRESS_KEY_LAST_ACCESSED = "last_accessed"
+
 
     def __init__(self, save_path):
         self.save_path = save_path
 
-    def api_request(self):
-        pass
+    def api_request(self, uri):
+        """Make a request for board game geek data.
 
-    def retrieve_all(self):
-        # Pseudo code:
-        # 1) Check if save_path file exists
-        # 2) If it exists, load a progress object from it.
-        # 3) If it doesn't exist, generate list of ids from 1 to max id,
-        #, then create a progress object that batchifies this
-        # 4) Loop through progress object, updating it and saving it everytime.
+        Parameters
+        ----------
+        uri : str
+            String URI for accessing the API.
 
-        pass
+        Returns
+        -------
+        Response object
+            The resulting response from the HTTP request.
+        """
+        r = requests.get(uri)
+        return r
+
+    def retrieve_all(self, server_cooldown=12*60*60):
+        # Resume from an existing progress file
+        # or create new progress object and batches.
+        if self.check_progress_file_exists():
+            progress = self.load_progress_file()
+        else:
+            ids = [i for i in range(1, self.MAX_ID + 1)]
+            progress = self.create_progress_object(ids, batch_size=1000)
+
+        self.save_progress_file(progress)  # Initial save
+        # Loop progress object, ignoring already complete batches.
+        for idx, batch in enumerate(progress.copy()):
+            if batch[self.PROGRESS_KEY_STATUS] == \
+                    self.PROGRESS_STATUS_COMPLETE:
+                continue
+            else:
+                # Try the request, but pause 10 min if no internet
+                while True:
+                    try:
+                        uri = generate_game_uri(batch[self.PROGRESS_KEY_IDS])
+                        r = self.api_request(uri)
+                        break
+                    except requests.ConnectionError:
+                        pause_time = 600
+                        print(f"Unable to connect to internet, "
+                              f"pausing {pause_time} seconds.")
+                        sleep(pause_time)
+                        continue
+                # If its 200, save the file, change status to complete
+                # If it's 202, mark it as queued.
+                # Anything else, could mean server blocking or down,
+                # so wait a while, then try again.
+                if r.status_code == 200:
+                    batch[self.PROGRESS_KEY_STATUS] == \
+                        self.PROGRESS_STATUS_COMPLETE
+                    progress[idx] = batch
+                    self.save_progress_file(progress)
+                if r.status_code == 202:
+                    batch[self.PROGRESS_KEY_STATUS] == \
+                        self.PROGRESS_STATUS_QUEUED
+                    progress[idx] = batch
+                    self.save_progress_file(progress)
+                else:
+                    message = (
+                        f"Response {r.status_code}. "
+                        f"Waiting {server_cooldown} seconds before continuing."
+                    )
+                    print(message)
+                    sleep(server_cooldown)
+
+            # Load progress file at the end and count up how many incompletes.
+            # Also, data actually has to be saved somehow.
 
     def create_progress_object(
             self,
