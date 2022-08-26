73fa471f64e604fcbe624e198ddc8965308e4144
Andrew Wong <wong.andrewko@gmail.com>
2022-08-26 16:18:52 -0700

    Refactor hard coded test variables into constants at test start

--- test/test_retrieve_all.py
+++ test/test_retrieve_all.py
@@ -17,10 +17,22 @@ class MockResponse:
 def test_200(monkeypatch, tmp_path):
     TEST_BATCH_SIZE = 2
     TEST_MAX_ID = 10
-    TEST_DIR = Path('./exp')  # or tmp_path
+    TEST_DIR = tmp_path  # system temporary files
+    TEST_STATUS_CODE = 200
+    TEST_RANDOM_SEED = 7
+    TEST_STATUS_STR = 'complete'
+    # Manually change this to true if you want to inspect
+    # the progress file.
+    # This will dump the files into the stated directory,
+    # as well as moving the progress file into a 'tmp_prog.json'
+    # at the end of this test.
+    INSPECT_PROGRESS_FILE = False
+    TEST_DIR_INSPECT = Path('./exp')
+    if INSPECT_PROGRESS_FILE:
+        TEST_DIR = TEST_DIR_INSPECT
 
     def mock_get(url):
-        return MockResponse(status_code=200)
+        return MockResponse(status_code=TEST_STATUS_CODE)
 
     monkeypatch.setattr(requests, 'get', mock_get)
 
@@ -29,7 +41,7 @@ def test_200(monkeypatch, tmp_path):
 
     retriever.retrieve_all(batch_size=TEST_BATCH_SIZE,
                            shuffle=True,
-                           random_seed=7)
+                           random_seed=TEST_RANDOM_SEED)
 
     progress_path = Path(retriever.progress_path)
 
@@ -44,6 +56,7 @@ def test_200(monkeypatch, tmp_path):
     # Test all status are 'complete'
     statuses = set([e[retriever.PROGRESS_KEY_STATUS] for e in progress])
     assert len(statuses) == 1
-    assert statuses.pop() == 'complete'
+    assert statuses.pop() == TEST_STATUS_STR
 
-    temp_progress = progress_path.replace(progress_path.parent / 'tmp_prog.json')
+    if INSPECT_PROGRESS_FILE:
+        progress_path.replace(progress_path.parent / 'tmp_prog.json')
