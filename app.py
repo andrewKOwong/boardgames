28e3ab876a0e04a9df0121fc494f90af4ea953dc
Andrew Wong <wong.andrewko@gmail.com>
2022-09-20 12:07:47 -0700

    Add: cached data loading func and calls

--- app.py
+++ app.py
@@ -6,74 +6,29 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from matplotlib.colors import XKCD_COLORS as xc
 import matplotlib as mpl
-import utils as u
+
+CURRENT_YEAR = 2022
+PATH_GENERAL_DATA = "./data/csv/2022-09-19_general_data.csv"
+PATH_LINK_DATA = "./data/csv/2022-09-19_link_data.csv"
+PATH_POLL_DATA = "./data/csv/2022-09-19_poll_data.csv"
 
 # Matplotlib configuration
 plt.style.use('ggplot')
 COL_CRIMSON = xc['xkcd:crimson']
 
-CURRENT_YEAR = 2022
-
-# Get all the xml file names
-xml_data_files = [
-    "./data/"+file for file in os.listdir("./data/") if file.endswith('.xml')]
-xml_data_files.sort()
-# Extract the data into a single dataframe
-data_kwargs = {'id': True, 'year_published': True}
-df = pd.concat([pd.DataFrame(u.extract_xml(file, **data_kwargs))
-               for file in xml_data_files], axis=0)
-
-# Raw stats
-st.write("## Raw Statistics")
-# Duplications
-st.write("### Duplications")
-dupes = st.columns(2)
-dupes[0].metric("Items", df.shape[0])
-dupes[1].metric("Duplicates", df.duplicated().sum())
-# Missing data
-st.write("### Missing Data")
-missing = st.columns(1)
-missing[0].metric("Year Published", (df['year_published'] == 0).sum())
-
-# Clean the df
-clean_df = df.drop_duplicates().query("year_published != 0")
-st.write("## Cleaned Statistics")
-st.write(clean_df.describe())
+@st.cache
+def load_data(path: str) -> pd.DataFrame:
+    return pd.read_csv(path)
 
 
-# Examine year published statistics
-st.write("## Games by Year Published")
+info_box = st.empty()
+with info_box.container():
+    st.info("Loading general data...")
+    gen_df = load_data(PATH_GENERAL_DATA)
+    st.info("Loading link data...")
+    link_df = load_data(PATH_LINK_DATA)
+    st.info("Loading poll data...")
+    poll_df = load_data(PATH_POLL_DATA)
 
+info_box.empty()
 
-def generate_year_histograms(df: pd.DataFrame) -> mpl.figure.Figure:
-    """Generates"""
-    FIG_SIZE = (6, 3)
-    YEAR_KEY = 'year_published'
-
-    fig, axes = plt.subplots(1, 2, figsize=FIG_SIZE)
-
-    # Histogram with all data
-    axes[0].hist(data=df,
-                 x=YEAR_KEY,
-                 bins=20,
-                 log=True,
-                 color=COL_CRIMSON
-                 )
-    # Histogram from 1980 onwards
-    axes[1].hist(data=df,
-                 x=YEAR_KEY,
-                 bins=np.arange(1979.5,
-                                df[YEAR_KEY].max() + 1,
-                                1),
-                 color=COL_CRIMSON)
-
-    # Set labels
-    for ax in axes:
-        ax.set_xlabel("Year")
-        ax.set_ylabel("Count")
-
-    fig.tight_layout()
-    return fig
-
-
-st.pyplot(generate_year_histograms(clean_df))
