566d353ec32d13bc29e5f47fed0713eb2faacefd
Andrew Wong <wong.andrewko@gmail.com>
2022-09-20 21:38:38 -0700

    Add code title text printer

--- core/analysis_utils.py
+++ core/analysis_utils.py
@@ -1,6 +1,25 @@
 from IPython.display import display, HTML
 
 
+def print_titled_text(
+        title: str,
+        underliner='=',
+        upper=True):
+    """Prints the text underlined to the same length with underliner char.
+
+    Args:
+        title (str): String to pring
+        underliner (str, optional): Char to underline the title.
+            Defaults to '='.
+        upper (bool, optional): Convert to all uppercase.
+            Defaults to True.
+    """
+    if upper:
+        title = title.upper()
+    print(title)
+    print(underliner*len(title))
+
+
 def display_id_link(id: int) -> None:
     """Display a clickable link in a Jupyter notebook from boardgame id.
 
