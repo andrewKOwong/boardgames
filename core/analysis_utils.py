6b90a14ecf1fa92c6cd5d31b9a5cbc39ed82de98
Andrew Wong <wong.andrewko@gmail.com>
2022-09-22 10:11:23 -0700

    Refactor: display link funcs to display names and multiples

--- core/analysis_utils.py
+++ core/analysis_utils.py
@@ -30,16 +30,36 @@ def print_spacer(num_lines: int = 2) -> None:
     print('\n'*num_lines)
 
 
-def display_id_link(id: int) -> None:
+def display_id_link(id: int, name: str = "") -> None:
     """Display a clickable link in a Jupyter notebook from boardgame id.
 
-    Parameters
-    ----------
-    id : int
-        The game id of interest.
+    Args:
+        id (int): Game id.
+        name (str, optional): Name of the game.
+            Defaults to "".
     """
     BASE_URL = "https://boardgamegeek.com/boardgame/"
-    display(HTML(f"<a href={BASE_URL + str(id)}>ID: {id}</a>"))
+    display(HTML(f"<a href={BASE_URL + str(id)}>ID: {id} {name}</a>"))
+
+
+def display_id_links_from_df(
+        df: pd.DataFrame,
+        id_key: str = 'id',
+        name_key: str = 'name') -> None:
+    """Display clickable links in a Jupyter notebook from a dataframe.
+
+    Best used with a small length dataframe (such as df.head() or df.samp(10)),
+    to keep print length reasonable.
+
+    Args:
+        df (pd.DataFrame): Dataframe to print from
+        id_key (str, optional): Key of id column.
+            Defaults to 'id'.
+        name_key (str, optional): Key of name column.
+            Defaults to 'name'.
+    """
+    for row in df.loc[:, [id_key, name_key]].iterrows():
+        display_id_link(row[1][id_key], name=row[1][name_key])
 
 
 def print_null_count(
